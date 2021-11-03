06a9a98205165a2081ca91550903926c5fe82137
Chris Jarvi <mybaldhead@hotmail.com>
2021-11-03 10:10:28 -0700

    Code along with course material

---
+++ app/app.ts
@@ -0,0 +1,20 @@
+import { Player } from './player';
+import { Game } from './game';
+import * as Helpers from './utility';
+
+let newGame: Game;
+
+document.getElementById('startGame')!.addEventListener('click', () => {
+    const player: Player = new Player();
+    player.name = Helpers.getValue('playername');
+
+    const problemCount: number = Number(Helpers.getValue('problemCount'));
+    const factor: number = Number(Helpers.getValue('factor'));
+
+    newGame = new Game(player, problemCount, factor);
+    newGame.displayGame();
+});
+
+document.getElementById('calculate')!.addEventListener('click', () => {
+    newGame.calculateScore();
+});
